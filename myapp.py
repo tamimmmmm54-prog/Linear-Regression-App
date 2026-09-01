9d11f19bc72c42a38cc2e0322b07dbee0bdae3d0
tamimmmmm54-prog <tamimmmmm54@gmail.com>
2026-09-01 17:53:17 +0600

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import Standardscaler
+from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
 st.title('Linear Regression Web Aplication')
