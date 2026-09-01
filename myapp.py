1b0d6cf2a2337fcd6f1db38d9ef964042c0725fb
tamimmmmm54-prog <tamimmmmm54@gmail.com>
2026-09-01 17:51:26 +0600

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,2 +1,9 @@
 import streamlit as st
+import pands as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import Standardscaler
+from sklearn.model_selection import train_test_split
+
 st.title('Linear Regression Web Aplication')
+st.subheader('Data Scirnce with Tamim')
