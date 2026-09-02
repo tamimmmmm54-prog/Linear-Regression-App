6c56f8cd72742844033b18acb30c9d7cdf68337d
tamimmmmm54-prog <tamimmmmm54@gmail.com>
2026-09-02 17:34:29 +0600

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -80,7 +80,7 @@ if st.button('Predict'):
     input_scaled = scaler.transform(input_df)
     prediction = model.predict(input_scaled)
     st.success(f'Predicted {target}: {Prediction[0]:.2f}')
-  else 
+  else: 
 
 st.error('Please Enter Valid numeric values for all features before prediction')
 
